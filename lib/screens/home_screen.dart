0445a80747ce3f66540b74adbbb78f184a72bb0c
hp <anisfatihahmurad@gmail.com>
2026-06-10 16:28:33 +0800

    feat: breathing exercises list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mindful_break/screens/breathingexercises_screen.dart';
 import 'package:mindful_break/screens/breathinglibrary_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -72,7 +73,7 @@ class HomeScreen extends StatelessWidget {
                                 context,
                                 MaterialPageRoute(
                                   builder: (_) =>
-                                  const BreathingLibraryScreen(),
+                                  const BreathingExercisesScreen(),
                                 ),
                               );
                             },
