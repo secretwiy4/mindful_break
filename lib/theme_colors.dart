e6feb097313c24accdae3cdf634aa1490c962361
miarisyaaaa <1411batrisyia@gmail.com>
2026-06-11 14:00:40 +0800

    sending theme_colors

---
+++ lib/theme_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color sageGreen = Color(0xFF67B7A4);
+  static const Color darkSage = Color(0xFF2D6A4F);
+  static const Color lightBg = Color(0xFFF7F9F8);
+  static const Color cardBg = Colors.white;
+  static const Color textDark = Color(0xFF222222);
+  static const Color textLight = Color(0xFF777777);
+}
